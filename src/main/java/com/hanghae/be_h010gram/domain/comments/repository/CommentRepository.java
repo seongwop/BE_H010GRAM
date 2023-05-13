523ad209391de8d7576e0ca1912082428d5dbdd9
Ma DongChan <69786324+mdc2684@users.noreply.github.com>
2023-05-14 00:24:33 +0900

    Delete src/main/java/com/hanghae/be_h010gram/domain/comments directory
    comments 없애기

--- src/main/java/com/hanghae/be_h010gram/domain/comments/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hanghae.be_h010gram.domain.comments.repository;
-
-import com.hanghae.be_h010gram.domain.comments.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
