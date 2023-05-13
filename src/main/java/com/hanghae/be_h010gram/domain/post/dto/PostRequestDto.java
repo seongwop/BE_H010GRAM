44b074424d30e2f0eccee9c5fc47fed3881bb4ff
LEOJI <kjh11-07@hanmail.net>
2023-05-13 10:50:07 +0900

    Feat : Post CRUD 기본틀 완성

--- src/main/java/com/hanghae/be_h010gram/domain/post/dto/PostRequestDto.java
+++ src/main/java/com/hanghae/be_h010gram/domain/post/dto/PostRequestDto.java
@@ -1,11 +1,11 @@
-package com.hanghae.be_h010gram.domain.posts.dto;
+package com.hanghae.be_h010gram.domain.post.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class PostsRequestDto {
+public class PostRequestDto {
     private String content;
 
 }
