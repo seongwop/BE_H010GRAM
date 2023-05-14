38700d3a9c5808b154d83d0d49e40f9498f09606
seongwop <psw921@gmail.com>
2023-05-14 21:11:07 +0900

    refactor : 좋아요 기능 domain 분리

--- src/main/java/com/hanghae/be_h010gram/domain/like/repository/CommentLikeRepository.java
+++ src/main/java/com/hanghae/be_h010gram/domain/like/repository/CommentLikeRepository.java
@@ -1,7 +1,7 @@
-package com.hanghae.be_h010gram.domain.comment.repository;
+package com.hanghae.be_h010gram.domain.like.repository;
 
 import com.hanghae.be_h010gram.domain.comment.entity.Comment;
-import com.hanghae.be_h010gram.domain.comment.entity.CommentLike;
+import com.hanghae.be_h010gram.domain.like.entity.CommentLike;
 import com.hanghae.be_h010gram.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
