af8a442ac182861e076c335908322eb7e7d83321
LEOJI <kjh11-07@hanmail.net>
2023-05-17 14:13:34 +0900

    Fix : CrossOrigin 어노테이션 추가

--- src/main/java/com/hanghae/be_h010gram/domain/like/controller/LikeController.java
+++ src/main/java/com/hanghae/be_h010gram/domain/like/controller/LikeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("api")
 @Tag(name = "like", description = "좋아요 API")
+@CrossOrigin
 public class LikeController {
 
     private final LikeService likeService;
