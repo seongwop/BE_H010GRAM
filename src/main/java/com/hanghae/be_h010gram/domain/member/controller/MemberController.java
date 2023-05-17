06d171ea8cb3af10a40fec12c0262f393b0f6113
cozy <wonje1108@gmail.com>
2023-05-17 16:27:42 +0900

    수정

--- src/main/java/com/hanghae/be_h010gram/domain/member/controller/MemberController.java
+++ src/main/java/com/hanghae/be_h010gram/domain/member/controller/MemberController.java
@@ -3,7 +3,7 @@ package com.hanghae.be_h010gram.domain.member.controller;
 import com.hanghae.be_h010gram.domain.member.dto.MemberRequestDto;
 import com.hanghae.be_h010gram.domain.member.dto.MemberResponseDto;
 import com.hanghae.be_h010gram.domain.member.dto.ProfileRequestDto;
-import com.hanghae.be_h010gram.domain.member.service.MemberService;
+import com.hanghae.be_h010gram.domain.member.Service.MemberService;
 import com.hanghae.be_h010gram.security.auth.UserDetailsImpl;
 import com.hanghae.be_h010gram.util.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
