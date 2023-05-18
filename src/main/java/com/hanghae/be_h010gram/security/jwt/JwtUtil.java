c4c1708d666da49519bf5c0ff9b0341920d2102f
cozy <wonje1108@gmail.com>
2023-05-18 14:57:57 +0900

    토큰 시간 수정

--- src/main/java/com/hanghae/be_h010gram/security/jwt/JwtUtil.java
+++ src/main/java/com/hanghae/be_h010gram/security/jwt/JwtUtil.java
@@ -25,7 +25,7 @@ public class JwtUtil {
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String BEARER_PREFIX = "Bearer ";
-    private static final long TOKEN_TIME = 60 * 60 * 1000L;
+    private static final long TOKEN_TIME = 24 * 60 * 60 * 1000L;
     private final UserDetailsServiceImpl userDetailsService;
 
     @Value("${jwt.secret.key}")
